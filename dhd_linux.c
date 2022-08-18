GrapheneOS/kernel_google-modules_wlan_bcmdhd_bcm4398

commit 15d84c3db8eabe049282222729302fbfdb3051fa

Fix for nan iface failure in kernel > 5.15
Included below changes:
1. Replaced wiphy_vendor_commands with the vendor_ops
2. Changed interface registration as per the new semantics.

Bug:226984257
Verify: Verified on pixel6 using the halutil

Change-Id: Id5052c2ebb8ea185019c020a8dd844750fcce5c5
Signed-off-by: Ajay Davanageri <ajay.davanageri@broadcom.corp-partner.google.com>

dhd_linux.c
@@ -5614,6 +5614,47 @@ EXPORT_SYMBOL(dhd_bus_retry_hang_recovery);
 
 #endif /* BT_OVER_SDIO */
 
+void dhd_unregister_net(struct net_device *net, bool need_rtnl_lock)
+{
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0))
+	if (need_rtnl_lock) {
+		rtnl_lock();
+		cfg80211_unregister_netdevice(net);
+		rtnl_unlock();
+	} else {
+		cfg80211_unregister_netdevice(net);
+	}
+#else
+	if (need_rtnl_lock) {
+		unregister_netdev(net);
+	} else {
+		unregister_netdevice(net);
+	}
+#endif /* KERNEL_VER >= KERNEL_VERSION(5, 15, 0) */
+	return;
+}
+
+int dhd_register_net(struct net_device *net, bool need_rtnl_lock)
+{
+	int err = 0;
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0))
+	if (need_rtnl_lock) {
+		rtnl_lock();
+		err = cfg80211_register_netdevice(net);
+		rtnl_unlock();
+	} else {
+		err = cfg80211_register_netdevice(net);
+	}
+#else
+	if (need_rtnl_lock) {
+		err = register_netdev(net);
+	} else {
+		err = register_netdevice(net);
+	}
+#endif /* KERNEL_VER >= KERNEL_VERSION(5, 15, 0) */
+	return err;
+}
+
 static int
 dhd_monitor_ioctl(struct net_device *dev, struct ifreq *ifr, int cmd)
 {
@@ -5694,7 +5735,8 @@ dhd_add_monitor_if(dhd_info_t *dhd)
 	 * So, register_netdev() shouldn't be called. It leads to deadlock.
 	 * To avoid deadlock due to rtnl_lock(), register_netdevice() should be used.
 	 */
-	if (register_netdevice(dev)) {
+	ret = dhd_register_net(dev, false);
+	if (ret) {
 		DHD_ERROR(("%s, register_netdev failed for %s\n",
 			__FUNCTION__, dev->name));
 		free_netdev(dev);
@@ -5771,11 +5813,7 @@ dhd_del_monitor_if(dhd_info_t *dhd)
 		if (dhd->monitor_dev->reg_state == NETREG_UNINITIALIZED) {
 			free_netdev(dhd->monitor_dev);
 		} else {
-			if (rtnl_is_locked()) {
-				unregister_netdevice(dhd->monitor_dev);
-			} else {
-				unregister_netdev(dhd->monitor_dev);
-			}
+			dhd_unregister_net(dhd->monitor_dev, !rtnl_is_locked());
 		}
 		dhd->monitor_dev = NULL;
 	}
@@ -5796,11 +5834,11 @@ dhd_set_monitor(dhd_pub_t *pub, int ifidx, int val)
 
 	dhd_net_if_lock_local(dhd);
 	if (!val) {
-			/* Delete monitor */
-			dhd_del_monitor_if(dhd);
+		/* Delete monitor */
+		dhd_del_monitor_if(dhd);
 	} else {
-			/* Add monitor */
-			dhd_add_monitor_if(dhd);
+		/* Add monitor */
+		dhd_add_monitor_if(dhd);
 	}
 	dhd->monitor_type[ifidx] = val;
 	dhd_net_if_unlock_local(dhd);
@@ -7743,10 +7781,7 @@ dhd_allocate_if(dhd_pub_t *dhdpub, int ifidx, const char *name,
 				free_netdev(ifp->net);
 			} else {
 				dhd_tx_stop_queues(ifp->net);
-				if (need_rtnl_lock)
-					unregister_netdev(ifp->net);
-				else
-					unregister_netdevice(ifp->net);
+				dhd_unregister_net(ifp->net, need_rtnl_lock);
 			}
 			ifp->net = NULL;
 		}
@@ -8007,10 +8042,7 @@ dhd_remove_if(dhd_pub_t *dhdpub, int ifidx, bool need_rtnl_lock)
 #if (defined(DHDTCPACK_SUPPRESS) && defined(BCMPCIE))
 				dhd_tcpack_suppress_set(dhdpub, TCPACK_SUP_OFF);
 #endif /* DHDTCPACK_SUPPRESS && BCMPCIE */
-				if (need_rtnl_lock)
-					unregister_netdev(ifp->net);
-				else
-					unregister_netdevice(ifp->net);
+				dhd_unregister_net(ifp->net, need_rtnl_lock);
 			}
 			ifp->net = NULL;
 		}
@@ -14433,11 +14465,7 @@ dhd_register_if(dhd_pub_t *dhdp, int ifidx, bool need_rtnl_lock)
 	if (ifidx == 0)
 		DHD_CONS_ONLY(("%s\n", dhd_version));
 
-	if (need_rtnl_lock)
-		err = register_netdev(net);
-	else
-		err = register_netdevice(net);
-
+	err = dhd_register_net(net, need_rtnl_lock);
 	if (err != 0) {
 		DHD_ERROR(("couldn't register the net device [%s], err %d\n", net->name, err));
 		goto fail;
@@ -14698,7 +14726,7 @@ void dhd_detach(dhd_pub_t *dhdp)
 				free_netdev(ifp->net);
 			} else {
 				netif_tx_disable(ifp->net);
-				unregister_netdev(ifp->net);
+				dhd_unregister_net(ifp->net, true);
 			}
 #ifdef PCIE_FULL_DONGLE
 			ifp->net = DHD_NET_DEV_NULL;

dhd.h
@@ -4959,6 +4959,8 @@ int dhd_config_rts_in_suspend(dhd_pub_t *dhdp, bool suspend);
 #endif /* DHD_CUSTOM_CONFIG_RTS_IN_SUSPEND */
 
 extern void *dhd_irq_to_desc(unsigned int irq);
+extern void dhd_unregister_net(struct net_device *net, bool need_rtnl_lock);
+extern int dhd_register_net(struct net_device *net, bool need_rtnl_lock);
 
 #ifdef TX_FLOW_RING_INDICES_TRACE
 uint32 dhd_prot_get_flow_ring_trace_len(dhd_pub_t *dhdp);

wl_android.c
@@ -26,6 +26,7 @@
 #include <linux/module.h>
 #include <linux/netdevice.h>
 #include <net/netlink.h>
+#include <net/rtnetlink.h>
 #ifdef CONFIG_COMPAT
 #include <linux/compat.h>
 #endif
@@ -14349,7 +14350,7 @@ wl_cfg80211_unregister_static_if(struct bcm_cfg80211 *cfg)
 	/* wdev free will happen from notifier context */
 	/* free_netdev(cfg->static_ndev);
 	*/
-	unregister_netdev(cfg->static_ndev);
+	dhd_unregister_net(cfg->static_ndev, true);
 }
 
 s32

wl_cfg80211.c
@@ -316,18 +316,9 @@ static const struct ieee80211_iface_limit common_if_limits[] = {
 	/*
 	 * During P2P-GO removal, P2P-GO is first changed to STA and later only
 	 * removed. So setting maximum possible number of STA interfaces according
-	 * to kernel version.
-	 *
-	 * less than linux-3.8 - max:3 (wlan0 + p2p0 + group removal of p2p-p2p0-x)
-	 * linux-3.8 and above - max:4
-	 * sta + NAN NMI + NAN DPI open + NAN DPI sec (since there is no iface type
-	 * for NAN defined, registering it as STA type)
+	 *  to kernel version. (2 sta + 1 sta for iface downgrade from ap)
 	 */
-#ifdef WL_ENABLE_P2P_IF
-	.max = 3,
-#else
 	.max = 4,
-#endif /* WL_ENABLE_P2P_IF */
 	.types = BIT(NL80211_IFTYPE_STATION),
 	},
 	{
@@ -2356,7 +2347,7 @@ wl_apply_vif_sta_config(struct bcm_cfg80211 *cfg,
 	}
 }
 
-void
+s32
 wl_cfg80211_iface_state_ops(struct wireless_dev *wdev,
 	wl_interface_state_t state,
 	wl_iftype_t wl_iftype, u16 wl_mode)
@@ -2368,12 +2359,15 @@ wl_cfg80211_iface_state_ops(struct wireless_dev *wdev,
 #endif /* CUSTOM_SET_CPUCORE || SUPPORT_AP_POWERSAVE */
 	s32 bssidx;
 	struct net_info *netinfo = NULL;
+	s32 idx = 0;
+
+	BCM_REFERENCE(idx);
 
 	WL_DBG(("state:%s wl_iftype:%d mode:%d\n",
 		wl_if_state_strs[state], wl_iftype, wl_mode));
 	if (!wdev) {
 		WL_ERR(("wdev null\n"));
-		return;
+		return -EINVAL;
 	}
 
 	if ((wl_iftype == WL_IF_TYPE_P2P_DISC) || (wl_iftype == WL_IF_TYPE_NAN_NMI)) {
@@ -2383,7 +2377,7 @@ wl_cfg80211_iface_state_ops(struct wireless_dev *wdev,
 		 * NAN NMI is netless device and uses a hidden bsscfg interface in fw.
 		 * Don't apply iface ops state changes for NMI I/F.
 		 */
-		return;
+		return BCME_OK;
 	}
 
 	cfg = wiphy_priv(wdev->wiphy);
@@ -2396,7 +2390,7 @@ wl_cfg80211_iface_state_ops(struct wireless_dev *wdev,
 	bssidx = wl_get_bssidx_by_wdev(cfg, wdev);
 	if (!ndev || (bssidx < 0)) {
 		WL_ERR(("ndev null. skip iface state ops\n"));
-		return;
+		return BCME_OK;
 	}
 
 	switch (state) {
@@ -2411,6 +2405,14 @@ wl_cfg80211_iface_state_ops(struct wireless_dev *wdev,
 			/* disable TDLS if number of connected interfaces is >= 1 */
 			wl_cfg80211_tdls_config(cfg, TDLS_STATE_IF_CREATE, false);
 #endif /* WLTDLS */
+#ifdef WL_NAN
+			if (wl_iftype == WL_IF_TYPE_NAN && IS_NDI_IFACE(ndev->name)) {
+				if ((idx = wl_cfgnan_get_ndi_idx(cfg)) < 0) {
+					WL_ERR(("No free idx for NAN NDI\n"));
+					return BCME_NORESOURCE;
+				}
+			}
+#endif /* WL_NAN */
 			break;
 		case WL_IF_DELETE_REQ:
 			if (netinfo && netinfo->passphrase_cfg) {
@@ -2432,7 +2434,15 @@ wl_cfg80211_iface_state_ops(struct wireless_dev *wdev,
 				dhd_set_cpucore(dhd, FALSE);
 			}
 #endif /* CUSTOM_SET_CPUCORE */
-			 wl_add_remove_pm_enable_work(cfg, WL_PM_WORKQ_DEL);
+			wl_add_remove_pm_enable_work(cfg, WL_PM_WORKQ_DEL);
+			wl_release_vif_macaddr(cfg, wdev->netdev->dev_addr, wl_iftype);
+#ifdef WL_NAN
+			if ((wl_iftype == WL_IF_TYPE_NAN) &&
+				(wl_cfgnan_del_ndi_data(cfg, wdev->netdev->name)) < 0) {
+				WL_ERR(("Failed to find matching data for ndi:%s\n",
+					wdev->netdev->name));
+			}
+#endif /* WL_NAN */
 			break;
 		case WL_IF_CREATE_DONE:
 			if (wl_mode == WL_MODE_BSS) {
@@ -2462,6 +2472,18 @@ wl_cfg80211_iface_state_ops(struct wireless_dev *wdev,
 				dhd_set_ap_powersave(dhd, 0, TRUE);
 #endif /* SUPPORT_AP_POWERSAVE && BCMDONGLEHOST */
 			}
+#ifdef WL_NAN
+			if (wl_iftype == WL_IF_TYPE_NAN) {
+				/* Store the iface name to pub data so that it can be used
+				 * during NAN enable
+				 */
+				if ((idx = wl_cfgnan_get_ndi_idx(cfg)) < 0) {
+					WL_ERR(("No free idx for NAN NDI\n"));
+					return BCME_NORESOURCE;
+				}
+				wl_cfgnan_add_ndi_data(cfg, idx, ndev->name, wdev);
+			}
+#endif /* WL_NAN */
 			break;
 		case WL_IF_DELETE_DONE:
 #ifdef WLTDLS
@@ -2486,8 +2508,9 @@ wl_cfg80211_iface_state_ops(struct wireless_dev *wdev,
 
 		default:
 			WL_ERR(("Unsupported state: %d\n", state));
-			return;
+			return BCME_OK;
 	}
+	return BCME_OK;
 }
 
 static s32
@@ -2758,6 +2781,20 @@ _wl_cfg80211_add_if(struct bcm_cfg80211 *cfg,
 		return NULL;
 	}
 
+#ifdef WL_NAN
+	if (wl_iftype == WL_IF_TYPE_STA && IS_NDI_IFACE(name)) {
+		/* Check for aware* iface name for NAN iftype */
+		if (cfg->nancfg->nan_init_state && cfg->nancfg->nan_enable) {
+			wl_iftype = WL_IF_TYPE_NAN;
+			WL_DBG(("NDI create req: iface name %s, change iftype to %d\n",
+				name, wl_iftype));
+		} else {
+			WL_ERR(("Nan must be inited/enabled\n"));
+			return NULL;
+		}
+	}
+#endif /* WL_NAN */
+
 	wiphy = bcmcfg_to_wiphy(cfg);
 #if defined(BCMDONGLEHOST)
 	dhd = (dhd_pub_t *)(cfg->pub);
@@ -2800,8 +2837,11 @@ _wl_cfg80211_add_if(struct bcm_cfg80211 *cfg,
 #endif /* DNGL_AXI_ERROR_LOGGING && REPORT_AXI_ERROR */
 	/* Protect the interace op context */
 	/* Do pre-create ops */
-	wl_cfg80211_iface_state_ops(primary_ndev->ieee80211_ptr, WL_IF_CREATE_REQ,
-		wl_iftype, wl_mode);
+	if ((err = wl_cfg80211_iface_state_ops(primary_ndev->ieee80211_ptr, WL_IF_CREATE_REQ,
+			wl_iftype, wl_mode)) < 0) {
+		WL_ERR(("Failed in state_ops: wl_iftype %d\n", wl_iftype));
+		return NULL;
+	}
 
 	if (strnicmp(name, SOFT_AP_IF_NAME, strlen(SOFT_AP_IF_NAME)) == 0) {
 		macaddr_iftype = WL_IF_TYPE_AP;
@@ -3094,21 +3134,11 @@ exit:
 		}
 		wl_cfg80211_iface_state_ops(primary_ndev->ieee80211_ptr,
 				WL_IF_DELETE_DONE, wl_iftype, wl_mode);
-		if (
-#ifdef WL_NAN
-			(!((cfg->nancfg->mac_rand) && (wl_iftype == WL_IF_TYPE_NAN))) &&
-#endif /* WL_NAN */
-#if defined(WL_STATIC_IF) && (defined(DHD_USE_RANDMAC) || defined(WL_SOFTAP_RAND))
-			/* interface delete is invalid for static interface */
-			(!IS_CFG80211_STATIC_IF(cfg, wdev_to_ndev(wdev))) &&
-#endif /* WL_STATIC_IF && (DHD_USE_RANDMAC || WL_SOFTAP_RAND) */
-			(TRUE))
-		{
-			wl_release_vif_macaddr(cfg, wdev->netdev->dev_addr, wl_iftype);
-		}
-		WL_INFORM_MEM(("vif deleted. vif_count:%d\n", cfg->vif_count));
 	} else {
-		if (!wdev->netdev) {
+		if (ret == -ENODEV) {
+			WL_INFORM(("Already deleted: %s\n", ifname));
+			ret = BCME_OK;
+		} else if (!wdev->netdev) {
 			WL_ERR(("ndev null! \n"));
 		} else {
 			/* IF del failed. revert back tx queue status */

wl_cfg80211.h
@@ -3629,7 +3629,7 @@ extern u32 wl_log_level;
 extern u32 wl_cfg80211_debug_data_dump(struct net_device *dev, u8 *buf, u32 buf_len);
 extern void wl_cfg80211_concurrent_roam(struct bcm_cfg80211 *cfg, int enable);
 
-extern void wl_cfg80211_iface_state_ops(struct wireless_dev *wdev, wl_interface_state_t state,
+extern s32 wl_cfg80211_iface_state_ops(struct wireless_dev *wdev, wl_interface_state_t state,
 	wl_iftype_t wl_iftype, u16 wl_mode);
 extern chanspec_t wl_cfg80211_get_shared_freq(struct wiphy *wiphy);
 #ifdef SUPPORT_SET_CAC

wl_cfgnan.c
@@ -48,7 +48,6 @@
 #include <bcmstdlib_s.h>
 
 #define NAN_RANGE_REQ_EVNT 1
-#define NAN_RAND_MAC_RETRIES 10
 #define NAN_SCAN_DWELL_TIME_DELTA_MS 10
 
 #ifdef WL_NAN_DISC_CACHE
@@ -72,7 +71,6 @@ void wl_cfgnan_data_remove_peer(struct bcm_cfg80211 *cfg,
         struct ether_addr *peer_addr);
 static void wl_cfgnan_send_stop_event(struct bcm_cfg80211 *cfg);
 static void wl_cfgnan_disable_cleanup(struct bcm_cfg80211 *cfg);
-static s32 wl_cfgnan_get_ndi_idx(struct bcm_cfg80211 *cfg);
 static int wl_cfgnan_init(struct bcm_cfg80211 *cfg);
 static int wl_cfgnan_deinit(struct bcm_cfg80211 *cfg, uint8 busstate);
 static void wl_cfgnan_update_dp_info(struct bcm_cfg80211 *cfg, bool add,
@@ -82,8 +80,6 @@ static void wl_cfgnan_data_set_peer_dp_state(struct bcm_cfg80211 *cfg,
 static nan_ndp_peer_t* wl_cfgnan_data_get_peer(struct bcm_cfg80211 *cfg,
 	struct ether_addr *peer_addr);
 static int wl_cfgnan_disable(struct bcm_cfg80211 *cfg);
-static s32 wl_cfgnan_del_ndi_data(struct bcm_cfg80211 *cfg, char *name);
-static s32 wl_cfgnan_add_ndi_data(struct bcm_cfg80211 *cfg, s32 idx, char *name);
 static int wl_nan_print_stats_tlvs(void *ctx, const uint8 *data, uint16 type, uint16 len);
 
 #ifdef RTT_SUPPORT
@@ -6866,47 +6862,6 @@ fail:
 	return ret;
 }
 
-static int
-wl_cfgnan_get_ndi_macaddr(struct bcm_cfg80211 *cfg, u8* mac_addr)
-{
-	int i = 0;
-	int ret = BCME_OK;
-	bool rand_mac = cfg->nancfg->mac_rand;
-	BCM_REFERENCE(i);
-
-	if (rand_mac) {
-		/* ensure nmi != ndi */
-		do {
-			RANDOM_BYTES(mac_addr, ETHER_ADDR_LEN);
-			/* restore mcast and local admin bits to 0 and 1 */
-			ETHER_SET_UNICAST(mac_addr);
-			ETHER_SET_LOCALADDR(mac_addr);
-			i++;
-			if (i == NAN_RAND_MAC_RETRIES) {
-				break;
-			}
-		} while (eacmp(cfg->nancfg->nan_nmi_mac, mac_addr) == 0);
-
-		if (i == NAN_RAND_MAC_RETRIES) {
-			if (eacmp(cfg->nancfg->nan_nmi_mac, mac_addr) == 0) {
-				WL_ERR(("\nCouldn't generate rand NDI which != NMI\n"));
-				ret = BCME_NORESOURCE;
-				goto fail;
-			}
-		}
-	} else {
-		if (wl_get_vif_macaddr(cfg, WL_IF_TYPE_NAN,
-			mac_addr) != BCME_OK) {
-			ret = -EINVAL;
-			WL_ERR(("Failed to get mac addr for NDI\n"));
-			goto fail;
-		}
-	}
-
-fail:
-	return ret;
-}
-
 int
 wl_cfgnan_data_path_iface_create_delete_handler(struct net_device *ndev,
 	struct bcm_cfg80211 *cfg, char *ifname, uint16 type, uint8 busstate)
@@ -6926,7 +6881,7 @@ wl_cfgnan_data_path_iface_create_delete_handler(struct net_device *ndev,
 				goto fail;
 			}
 
-			ret = wl_cfgnan_get_ndi_macaddr(cfg, mac_addr);
+			ret = wl_get_vif_macaddr(cfg, WL_IF_TYPE_NAN, mac_addr);
 			if (ret != BCME_OK) {
 				WL_ERR(("Couldn't get mac addr for NDI ret %d\n", ret));
 				goto fail;
@@ -6941,18 +6896,11 @@ wl_cfgnan_data_path_iface_create_delete_handler(struct net_device *ndev,
 			/* Store the iface name to pub data so that it can be used
 			 * during NAN enable
 			 */
-			wl_cfgnan_add_ndi_data(cfg, idx, ifname);
-			cfg->nancfg->ndi[idx].created = true;
-			/* Store nan ndev */
-			cfg->nancfg->ndi[idx].nan_ndev = wdev_to_ndev(wdev);
-
+			wl_cfgnan_add_ndi_data(cfg, idx, ifname, wdev);
 		} else if (type == NAN_WIFI_SUBCMD_DATA_PATH_IFACE_DELETE) {
 			ret = wl_cfg80211_del_if(cfg, ndev, NULL, ifname);
 			if (ret == BCME_OK) {
-				if (wl_cfgnan_del_ndi_data(cfg, ifname) < 0) {
-					WL_ERR(("Failed to find matching data for ndi:%s\n",
-					ifname));
-				}
+				/* handled the post del ndi ops in _wl_cfg80211_del_if */
 			} else if (ret == -ENODEV) {
 				WL_INFORM(("Already deleted: %s\n", ifname));
 				ret = BCME_OK;
@@ -9807,7 +9755,7 @@ wl_cfgnan_is_dp_active(struct net_device *ndev)
 	return nan_dp;
 }
 
-static s32
+s32
 wl_cfgnan_get_ndi_idx(struct bcm_cfg80211 *cfg)
 {
 	int i;
@@ -9821,13 +9769,14 @@ wl_cfgnan_get_ndi_idx(struct bcm_cfg80211 *cfg)
 	return WL_INVALID;
 }
 
-static s32
-wl_cfgnan_add_ndi_data(struct bcm_cfg80211 *cfg, s32 idx, char *name)
+void
+wl_cfgnan_add_ndi_data(struct bcm_cfg80211 *cfg, s32 idx, char const *name,
+	struct wireless_dev *wdev)
 {
 	u16 len;
 	wl_nancfg_t *nancfg = cfg->nancfg;
 	if (!name || (idx < 0) || (idx >= cfg->nancfg->max_ndi_supported)) {
-		return -EINVAL;
+		return;
 	}
 
 	/* Ensure ifname string size <= IFNAMSIZ including null termination */
@@ -9835,13 +9784,13 @@ wl_cfgnan_add_ndi_data(struct bcm_cfg80211 *cfg, s32 idx, char *name)
 	strncpy(nancfg->ndi[idx].ifname, name, len);
 	nancfg->ndi[idx].ifname[len] = '\0';
 	nancfg->ndi[idx].in_use = true;
-	nancfg->ndi[idx].created = false;
-
-	/* Don't have a free interface */
-	return WL_INVALID;
+	nancfg->ndi[idx].created = true;
+	/* Store nan ndev */
+	cfg->nancfg->ndi[idx].nan_ndev = wdev_to_ndev(wdev);
+	return;
 }
 
-static s32
+s32
 wl_cfgnan_del_ndi_data(struct bcm_cfg80211 *cfg, char *name)
 {
 	u16 len;
@@ -10205,8 +10154,8 @@ wl_cfgnan_register_nmi_ndev(struct bcm_cfg80211 *cfg)
 	wdev->netdev = ndev;
 	wdev->wiphy = bcmcfg_to_wiphy(cfg);
 	wdev->iftype = NL80211_IFTYPE_STATION;
-
-	ret = register_netdev(ndev);
+ 
+	ret = dhd_register_net(ndev, true);
 	if (ret) {
 		WL_ERR((" NMI register_netdevice failed (%d)\n", ret));
 		goto fail;
@@ -10240,7 +10189,7 @@ wl_cfgnan_unregister_nmi_ndev(struct bcm_cfg80211 *cfg)
 		goto free_wdev;
 	}
 
-	unregister_netdev(cfg->nmi_ndev);
+	dhd_unregister_net(cfg->nmi_ndev, true);
 	free_netdev(cfg->nmi_ndev);
 
 	cfg->nmi_ndev = NULL;

wl_cfgnan.h
@@ -230,6 +230,8 @@
 */
 #define NAN_DISC_BCN_INTERVAL_2G_DEF 128u
 #define NAN_DISC_BCN_INTERVAL_5G_DEF 176u
+#define NAN_RAND_MAC_RETRIES 10
+#define IS_NDI_IFACE(ifname) strstr(ifname, "aware")
 
 typedef uint32 nan_data_path_id;
 
@@ -871,6 +873,10 @@ bool wl_cfgnan_ranging_is_in_prog_for_peer(struct bcm_cfg80211 *cfg,
 #else
 static INLINE bool wl_cfgnan_ranging_allowed(struct bcm_cfg80211 *cfg) { return FALSE; }
 #endif /* RTT_SUPPORT */
+extern s32 wl_cfgnan_get_ndi_idx(struct bcm_cfg80211 *cfg);
+extern void wl_cfgnan_add_ndi_data(struct bcm_cfg80211 *cfg, s32 idx,
+	char const *name, struct wireless_dev *wdev);
+extern s32 wl_cfgnan_del_ndi_data(struct bcm_cfg80211 *cfg, char *name);
 
 typedef enum {
 	NAN_ATTRIBUTE_INVALID				= 0,

wl_cfgp2p.c
@@ -2577,7 +2577,7 @@ wl_cfgp2p_register_ndev(struct bcm_cfg80211 *cfg)
 	wdev->netdev = net;
 #endif /* WL_NEWCFG_PRIVCMD_SUPPORT */
 
-	ret = register_netdev(net);
+	ret = dhd_register_net(net, true);
 	if (ret) {
 		CFGP2P_ERR((" register_netdevice failed (%d)\n", ret));
 		free_netdev(net);
@@ -2611,7 +2611,7 @@ wl_cfgp2p_unregister_ndev(struct bcm_cfg80211 *cfg)
 		return -EINVAL;
 	}
 
-	unregister_netdev(cfg->p2p_net);
+	dhd_unregister_net(cfg->p2p_net, true);
 	free_netdev(cfg->p2p_net);
 
 	return 0;

commit b3cddae9f1b296d8cceb264d02ee2e96534999a3

Fix RTNL assert when calling netdev_update_features()
The kernel requires the RTNL lock to be held when calling
netdev_update_features(). So do that to fix the RNTL assert.

Fixes: 15d84c3db8ea ("Fix for nan iface failure in kernel > 5.15")
Signed-off-by: Will McVicker <willmcvicker@google.com>
Change-Id: I70710c631e2b76cad1762c91d81123d2b9a83566

dhd_linux.c
@@ -3369,7 +3369,13 @@ dhd_ndev_upd_features_handler(void *handle, void *event_info, u8 event)
 		DHD_ERROR(("%s: event data is null \n", __FUNCTION__));
 		return;
 	}
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0))
+	rtnl_lock();
+#endif /* (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)) */
 	netdev_update_features(net);
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0))
+	rtnl_unlock();
+#endif /* (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)) */
 }
 
 static void