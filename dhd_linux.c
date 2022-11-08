505a06944e859ddfcdd2c76e6c0d850f3e4d5a4c
Dennis Jeon <dennis.jeon@broadcom.corp-partner.google.com>
2022-11-08 21:04:07 +0900

    bcmdhd: caches the memdump_type before accessing it in other contexts
    the signature is displayed incorrectly when fw trap occurs.
    
    This is an issue that occurs because the timing of the operation is changed due to the change.
    During the 1st dhd_mem_dump (fw trap), 2nd dhd_mem_dump(GET_RING_DATA) is scheduled with overwriting memdump_type from DUMP_TYPE_DONGLE_TRAP to DUMP_TYPE_BY_SYSDUMP.
    
    DHD schedules dhd_mem_dump() after assigning "dhdp->memdump_type"
    so, memdump_type can be modified during dhd_mem_dump() in one context when scheduling dhd_mem_dump() in another context.
    
    Bug: 257024419
    Test: FW trap test with "wl bus:disconnect 99" and it display the signature properly.
    
    Signed-off-by: Dennis Jeon <dennis.jeon@broadcom.corp-partner.google.com>
    Change-Id: I7f49642206c7491587d5895c717e19ba65d66ed5

--- dhd_linux.c
+++ dhd_linux.c
@@ -20079,6 +20079,7 @@ dhd_mem_dump(void *handle, void *event_info, u8 event)
 	char lr_fn[DHD_FUNC_STR_LEN] = "\0";
 	trap_t *tr;
 #endif /* DHD_COREDUMP */
+	uint32 memdump_type;
 
 	DHD_PRINT(("%s: ENTER \n", __FUNCTION__));
 
@@ -20092,6 +20093,9 @@ dhd_mem_dump(void *handle, void *event_info, u8 event)
 		DHD_ERROR(("%s: dhdp is NULL\n", __FUNCTION__));
 		return;
 	}
+
+	/* keep it locally to avoid overwriting in other contexts */
+	memdump_type = dhdp->memdump_type;
 
 	DHD_GENERAL_LOCK(dhdp, flags);
 	if (DHD_BUS_CHECK_DOWN_OR_DOWN_IN_PROGRESS(dhdp)) {
@@ -20184,9 +20188,9 @@ dhd_mem_dump(void *handle, void *event_info, u8 event)
 
 #ifdef DHD_COREDUMP
 	bzero(dhdp->memdump_str, DHD_MEMDUMP_LONGSTR_LEN);
-	dhd_convert_memdump_type_to_str(dhdp->memdump_type, dhdp->memdump_str,
+	dhd_convert_memdump_type_to_str(memdump_type, dhdp->memdump_str,
 		DHD_MEMDUMP_LONGSTR_LEN, dhdp->debug_dump_subcmd);
-	if (dhdp->memdump_type == DUMP_TYPE_DONGLE_TRAP &&
+	if (memdump_type == DUMP_TYPE_DONGLE_TRAP &&
 		dhdp->dongle_trap_occured == TRUE) {
 		tr = &dhdp->last_trap_info;
 		dhd_lookup_map(dhdp->osh, map_path,
@@ -20206,8 +20210,7 @@ dhd_mem_dump(void *handle, void *event_info, u8 event)
 			__FUNCTION__));
 	}
 #endif /* DHD_SSSR_COREDUMP */
-	if ((dhdp->memdump_type == DUMP_TYPE_BY_SYSDUMP) &&
-		(dhdp->dongle_trap_occured == false)) {
+	if (memdump_type == DUMP_TYPE_BY_SYSDUMP) {
 		DHD_LOG_MEM(("%s: coredump is not supported for BY_SYSDUMP/non trap cases\n",
 			__FUNCTION__));
 	} else {
@@ -20293,7 +20296,7 @@ dhd_mem_dump(void *handle, void *event_info, u8 event)
 	*/
 #ifdef DHD_LOG_DUMP
 	if (dhd->scheduled_memdump &&
-		dhdp->memdump_type != DUMP_TYPE_BY_SYSDUMP) {
+		memdump_type != DUMP_TYPE_BY_SYSDUMP) {
 		log_dump_type_t *flush_type = MALLOCZ(dhdp->osh,
 				sizeof(log_dump_type_t));
 		if (flush_type) {
@@ -20329,16 +20332,16 @@ dhd_mem_dump(void *handle, void *event_info, u8 event)
 
 	if (dhd->pub.memdump_enabled == DUMP_MEMFILE_BUGON &&
 #ifdef DHD_LOG_DUMP
-		dhd->pub.memdump_type != DUMP_TYPE_BY_SYSDUMP &&
+		memdump_type != DUMP_TYPE_BY_SYSDUMP &&
 #endif /* DHD_LOG_DUMP */
-		dhd->pub.memdump_type != DUMP_TYPE_BY_USER &&
+		memdump_type != DUMP_TYPE_BY_USER &&
 #ifdef DHD_DEBUG_UART
 		dhd->pub.memdump_success == TRUE &&
 #endif	/* DHD_DEBUG_UART */
 #ifdef DNGL_EVENT_SUPPORT
-		dhd->pub.memdump_type != DUMP_TYPE_DONGLE_HOST_EVENT &&
+		memdump_type != DUMP_TYPE_DONGLE_HOST_EVENT &&
 #endif /* DNGL_EVENT_SUPPORT */
-		dhd->pub.memdump_type != DUMP_TYPE_CFG_VENDOR_TRIGGERED) {
+		memdump_type != DUMP_TYPE_CFG_VENDOR_TRIGGERED) {
 #ifdef SHOW_LOGTRACE
 		/* Wait till logtrace context is flushed */
 		dhd_flush_logtrace_process(dhd);
