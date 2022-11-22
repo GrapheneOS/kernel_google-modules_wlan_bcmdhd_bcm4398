acc9d97e19d2712a1e4d210f059c50edcaa37ffa
Carlos Llamas <cmllamas@google.com>
2022-11-22 17:52:04 +0000

    bcmdhd: fix void argument for -Wstrict-prototypes
    Fixes several build errors similar to the following:
    
      wl_cfg_btcoex.c:439:37: error: a function declaration without a \
          prototype is deprecated in all versions of C \
          [-Werror,-Wstrict-prototypes]
      void wl_cfg80211_btcoex_kill_handler()
                                          ^
                                          void
    
    Bug: 255532233
    Change-Id: Iee06b05209ca128a31e51510a97722dc06942c6c
    Signed-off-by: Carlos Llamas <cmllamas@google.com>

--- wl_cfg_btcoex.c
+++ wl_cfg_btcoex.c
@@ -436,7 +436,7 @@ void* wl_cfg80211_btcoex_init(struct net_device *ndev)
 	return btco_inf;
 }
 
-void wl_cfg80211_btcoex_kill_handler()
+void wl_cfg80211_btcoex_kill_handler(void)
 {
 	if (!btcoex_info_loc)
 		return;
@@ -449,7 +449,7 @@ void wl_cfg80211_btcoex_kill_handler()
 	wl_cfg80211_btcoex_init_handler_status();
 }
 
-void wl_cfg80211_btcoex_deinit()
+void wl_cfg80211_btcoex_deinit(void)
 {
 	if (!btcoex_info_loc)
 		return;
